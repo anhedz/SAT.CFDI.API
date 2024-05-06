ec1c7aaafc6d3d8feb1d113ad3ba1304ec6c236c
Antonio Hernandez <anhedz@gmail.com>
2024-05-06 16:14:39 -0600

    documentacion

--- src/CRET/AutenticaService.cs
+++ src/CRET/AutenticaService.cs
@@ -1,6 +1,9 @@
 ﻿using Jaeger.SAT.API.Services.Interfaces;
 
 namespace Jaeger.SAT.API.CRET {
+    /// <summary>
+    /// servicio de autenticacion
+    /// </summary>
     internal class AutenticaService : API.Services.Abstracts.AutenticaService, IAutenticaService {
         public AutenticaService() {
             this.AddURL("https://retendescargamasivasolicitud.clouda.sat.gob.mx/Autenticacion/Autenticacion.svc");
